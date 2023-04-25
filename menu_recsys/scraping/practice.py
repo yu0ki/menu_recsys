df244b83aa17853ad27cf75368e6d894c3a16064
yu0ki <49509576+yu0ki@users.noreply.github.com>
2023-04-25 16:37:12 +0900

    [mod] メニュー詳細取得関数作成

--- menu_recsys/scraping/practice.py
+++ menu_recsys/scraping/practice.py
@@ -2,131 +2,170 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url = "https://west2-univ.jp/sp/detail.php?t=650111&c=814167"
-# url = "https://weathernews.jp/onebox/35.050126/135.787960/q=%E4%BA%AC%E9%83%BD%E5%B8%82%E5%B7%A6%E4%BA%AC%E5%8C%BA&v=f755dcb3f2c5eb660e33d08c2f6361a752e096e0c95ed7094a524287b7e0b0b1&temp=c&lang=ja"
-res = requests.get(url)
-if not res.ok:
-    print(f"ページの取得に失敗しました。status: {res.status_code}, reason: {res.reason}")
-else:
-    html = res.content
-    soup = BeautifulSoup(html)
+# メニュー詳細ページから、１つのメニューの詳細情報を取得する関数
+# 入力：メニューURL
+def get_menu_info(url: str):
 
-    # 商品名
-    dish_name = soup.select("#main > h1")[0].contents[0]
-    print("dish_name: ", dish_name)
+    # url = "https://west2-univ.jp/sp/detail.php?t=650111&c=814167"
+    res = requests.get(url)
+    if not res.ok:
+        print(f"ページの取得に失敗しました。status: {res.status_code}, reason: {res.reason}")
+    else:
+        html = res.content
+        soup = BeautifulSoup(html)
 
-    # 商品名英語
-    dish_en_name = soup.select("#main > h1")[0].contents[1].contents[0]
-    print("dish_en_name: ", dish_en_name)
+        # 商品名
+        dish_name = soup.select("#main > h1")[0].contents[0]
+        # print("dish_name: ", dish_name)
 
-    # 商品画像
-    image_url = soup.select("#main > div.menuImgBox > img")[0]["src"]
-    print("image_url: ", image_url)
+        # 商品名英語
+        dish_en_name = soup.select("#main > h1")[0].contents[1].contents[0]
+        # print("dish_en_name: ", dish_en_name)
 
-    # 値段（税込）
-    price = soup.select(
-        "#main > ul > li:nth-child(1) > span.price > strong"
-    )[0].contents[0]
-    price = re.sub(r"\D", "", price)
-    print("price: ", price)
-    
-    # エネルギー（kcal）
-    energy = soup.select(
-        "#main > ul > li:nth-child(2) > span.price"
-    )[0].contents[0]
-    energy = re.sub(r"\D", "", energy)
-    print("energy: ", energy)
+        # 商品画像
+        image_url = soup.select("#main > div.menuImgBox > img")[0]["src"]
+        # print("image_url: ", image_url)
 
-    # タンパク質（g）
-    protein = soup.select(
-        "#main > ul > li:nth-child(3) > span.price"
-    )[0].contents[0]
-    protein = re.sub(r"\D", "", protein)
-    print("protein: ", protein)
+        # 値段（税込）
+        price = soup.select(
+            "#main > ul > li:nth-child(1) > span.price > strong"
+        )[0].contents[0]
+        price = re.sub(r"[^0-9.-]", "", price)
+        # print("price: ", price)
+        
+        # エネルギー（kcal）
+        energy = soup.select(
+            "#main > ul > li:nth-child(2) > span.price"
+        )[0].contents[0]
+        energy = re.sub(r"[^0-9.-]", "", energy)
+        # print("energy: ", energy)
 
-    # 脂質（g）
-    fat= soup.select(
-        "#main > ul > li:nth-child(4) > span.price"
-    )[0].contents[0]
-    fat = re.sub(r"\D", "", fat)
-    print("fat: ", fat)
+        # タンパク質（g）
+        protein = soup.select(
+            "#main > ul > li:nth-child(3) > span.price"
+        )[0].contents[0]
+        protein = re.sub(r"[^0-9.-]", "", protein)
+        # print("protein: ", protein)
 
-    # 炭水化物（g）
-    carbohydrates = soup.select(
-        "#main > ul > li:nth-child(5) > span.price"
-    )[0].contents[0]
-    carbohydrates = re.sub(r"\D", "",carbohydrates)
-    print("carbohydrates: ", carbohydrates)
+        # 脂質（g）
+        fat= soup.select(
+            "#main > ul > li:nth-child(4) > span.price"
+        )[0].contents[0]
+        fat = re.sub(r"[^0-9.-]", "", fat)
+        # print("fat: ", fat)
 
-    # 食塩相当量（g）
-    salt = soup.select(
-        "#main > ul > li:nth-child(6) > span.price"
-    )[0].contents[0]
-    salt = re.sub(r"\D", "", salt)
-    print("salt: ", salt)
+        # 炭水化物（g）
+        carbohydrates = soup.select(
+            "#main > ul > li:nth-child(5) > span.price"
+        )[0].contents[0]
+        carbohydrates = re.sub(r"[^0-9.-]", "",carbohydrates)
+        # print("carbohydrates: ", carbohydrates)
 
-    # カルシウム（mg）
-    calcium = soup.select(
-        "#main > ul > li:nth-child(7) > span.price"
-    )[0].contents[0]
-    calcium = re.sub(r"\D", "", calcium)
-    print("calcium: ", calcium)
+        # 食塩相当量（g）
+        salt = soup.select(
+            "#main > ul > li:nth-child(6) > span.price"
+        )[0].contents[0]
+        salt = re.sub(r"[^0-9.-]", "", salt)
+        # print("salt: ", salt)
 
-    # 野菜量（g）
-    veg = soup.select(
-        "#main > ul > li:nth-child(8) > span.price"
-    )[0].contents[0]
-    veg = re.sub(r"\D", "", veg)
-    print("veg: ", veg)
+        # カルシウム（mg）
+        calcium = soup.select(
+            "#main > ul > li:nth-child(7) > span.price"
+        )[0].contents[0]
+        calcium = re.sub(r"[^0-9.-]", "", calcium)
+        # print("calcium: ", calcium)
 
-    # 鉄（mg）
-    iron = soup.select(
-        "#main > ul > li:nth-child(9) > span.price"
-    )[0].contents[0]
-    iron = re.sub(r"\D", "", iron)
-    print("iron: ", iron)
+        # 野菜量（g）
+        veg = soup.select(
+            "#main > ul > li:nth-child(8) > span.price"
+        )[0].contents[0]
+        veg = re.sub(r"[^0-9.-]", "", veg)
+        # print("veg: ", veg)
 
-    # ビタミンA（μg）
-    vitamin_a = soup.select(
-        "#main > ul > li:nth-child(10) > span.price"
-    )[0].contents[0]
-    vitamin_a = re.sub(r"\D", "", vitamin_a)
-    print("vitamin_a: ", vitamin_a)
+        # 鉄（mg）
+        iron = soup.select(
+            "#main > ul > li:nth-child(9) > span.price"
+        )[0].contents[0]
+        iron = re.sub(r"[^0-9.-]", "", iron)
+        # print("iron: ", iron)
 
-
-    # ビタミンB1（mg）
-    vitamin_b1 = soup.select(
-        "#main > ul > li:nth-child(11) > span.price"
-    )[0].contents[0]
-    vitamin_b1 = re.sub(r"\D", "", vitamin_b1)
-    print("vitamin_b1: ", vitamin_b1)
-    
-
-    # ビタミン B2（mg）
-    vitamin_b2 = soup.select(
-        "#main > ul > li:nth-child(12) > span.price"
-    )[0].contents[0]
-    vitamin_b2 = re.sub(r"\D", "", vitamin_b2)
-    print("vitamin_b2: ", vitamin_b2)
-    
-
-    # ビタミンC（16mg）
-    vitamin_c = soup.select(
-        "#main > ul > li:nth-child(13) > span.price"
-    )[0].contents[0]
-    vitamin_c = re.sub(r"\D", "", vitamin_c)
-    print("vitamin_c: ", vitamin_c)
-    
-
-    # 原産地（一応・・・けど使いにくいデータ形式）
-    place_of_origin = soup.select(
-        "#main > ul > li:nth-child(14) > span.price"
-    )[0].contents[0]
-    print("place_of_origin: ", place_of_origin)
+        # ビタミンA（μg）
+        vitamin_a = soup.select(
+            "#main > ul > li:nth-child(10) > span.price"
+        )[0].contents[0]
+        vitamin_a = re.sub(r"[^0-9.-]", "", vitamin_a)
+        # print("vitamin_a: ", vitamin_a)
 
 
+        # ビタミンB1（mg）
+        vitamin_b1 = soup.select(
+            "#main > ul > li:nth-child(11) > span.price"
+        )[0].contents[0]
+        vitamin_b1 = re.sub(r"[^0-9.-]", "", vitamin_b1)
+        # print("vitamin_b1: ", vitamin_b1)
+        
 
-    
+        # ビタミン B2（mg）
+        vitamin_b2 = soup.select(
+            "#main > ul > li:nth-child(12) > span.price"
+        )[0].contents[0]
+        vitamin_b2 = re.sub(r"[^0-9.-]", "", vitamin_b2)
+        # print("vitamin_b2: ", vitamin_b2)
+        
+
+        # ビタミンC（16mg）
+        vitamin_c = soup.select(
+            "#main > ul > li:nth-child(13) > span.price"
+        )[0].contents[0]
+        vitamin_c = re.sub(r"[^0-9.-]", "", vitamin_c)
+        # print("vitamin_c: ", vitamin_c)
+        
+
+        # 原産地（一応・・・けど使いにくいデータ形式）
+        place_of_origin = soup.select(
+            "#main > ul > li:nth-child(14) > span.price"
+        )[0].contents[0]
+        # print("place_of_origin: ", place_of_origin)
 
 
-    
+        # アレルギー（リスト形式）
+        allergies = []
+        allegy_list = soup.select(
+            "#main > ul > li.allergy > div > ul"
+        )[0].find_all("li")
+
+        for allegy in allegy_list:
+            allergies.append(allegy["class"][0][5:])
+        # print("allergies", allergies)
+
+        return {
+            "dish_name": dish_name,
+            "dish_en_name": dish_en_name,
+            "image_url": image_url,
+            "price": price,
+            "energy": energy,
+            "protein": protein,
+            "fat": fat,
+            "carbohydrates": carbohydrates,
+            "salt": salt,
+            "calcium": calcium,
+            "veg": veg,
+            "iron": iron,
+            "vitamin_a": vitamin_a,
+            "vitamin_b1": vitamin_b1,
+            "vitamin_b2": vitamin_b2,
+            "vitamin_c": vitamin_c,
+            "place_of_origin": place_of_origin,
+            "allergies": allergies,
+        }
+
+
+
+
+
+
+
+        
+
+
+        
