c1113d7b238323024817724e1a04bbdb3f3378b3
Honoka-Tanaka <130822382+Honoka-Tanaka@users.noreply.github.com>
2023-05-02 15:30:16 +0900

    getweatherinfo

---
+++ menu_recsys/scraping/weather.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+def get_weather_info():
+    
+    url = "https://weather.yahoo.co.jp/weather/jp/26/6110.html"
+    res = requests.get(url)
+    if not res.ok:
+        print(f"ページの取得に失敗しました。status: {res.status_code}, reason: {res.reason}")
+    else:
+        html = res.content
+        soup = BeautifulSoup(html)
+        # print(soup)
+        weather = soup.select("#main > div.forecastCity > table > tr > td > div > p.pict > img")[0].attrs["alt"]
+
+        elems = soup.select("#main > div.forecastCity > table >tr > td > div > ul")[0].find_all("li")
+        print(elems)
+        max_temperature = elems[0].select("li > em")[0].contents
+        min_temperature = elems[1].select("li > em")[0].contents
+        print(max_temperature)
+        print(min_temperature)
+        
+        return {
+            "weather": weather,
+            "max_temperature": max_temperature,
+            "min_temperature": min_temperature
+        }
