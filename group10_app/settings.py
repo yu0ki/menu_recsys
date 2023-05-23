440d27e2095a769a5a9305bdc9e6dc78c2c6ebda
yu0ki <49509576+yu0ki@users.noreply.github.com>
2023-05-23 17:32:35 +0900

    [del]イランファイル

--- group10_app/settings.py
+++ group10_app/settings.py
@@ -73,22 +73,22 @@ WSGI_APPLICATION = "group10_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'menu_recsys',  # database name
-        'USER': 'root',  # your database account
-        'PASSWORD': 'YM&2000?lqs=7011',  # your database password
-        'HOST': '127.0.0.1',  # your host
-        'PORT': 3306,
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'menu_recsys',  # database name
+#         'USER': 'root',  # your database account
+#         'PASSWORD': 'YM&2000?lqs=7011',  # your database password
+#         'HOST': '127.0.0.1',  # your host
+#         'PORT': 3306,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
